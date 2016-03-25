fe0fb1d9a2288863f900b28cae616d50b5d73325
partheey-vakilsearch <partheey@vakilsearch.com>
2016-03-25 17:27:01 +0530

    Added paperclip attachment

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,6 @@ class Profile < ActiveRecord::Base
 	belongs_to :user
 	has_many :family_members
 	accepts_nested_attributes_for :family_members, allow_destroy: true
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
