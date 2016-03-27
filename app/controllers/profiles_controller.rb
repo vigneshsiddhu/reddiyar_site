f02a0975463056a56c87280c4db62000fd59609c
vigneshsiddhu94 <vignesh.vakilsearch@gmail.com>
2016-03-27 16:54:42 +0530

    added data changes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -66,7 +66,7 @@ class ProfilesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_profile
-      @profile = Profile.find(params[:id])
+      @profile = current_user.profile
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
