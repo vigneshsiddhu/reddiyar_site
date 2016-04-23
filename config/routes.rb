b956f81d48526cc80b729c36cd875738a73ef112
partheey <partheey@vakilsearch.com>
2016-04-24 05:13:19 +0530

    Minor Changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :user_roles
   devise_for :users
   resources :family_members
-  resources :profiles
+  resources :profiles, only: [:show, :edit, :update, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
