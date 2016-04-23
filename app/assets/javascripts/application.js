020bb2ae8d0ac61e4848dfd7cf9ab0ec477df1b7
vigneshsiddhu94 <vignesh.vakilsearch@gmail.com>
2016-04-23 13:18:44 +0530

    design new integration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,4 +21,7 @@
 //= require_tree .
 //= require jquery_nested_form
 //= require bootstrap.min
+//= require metisMenu.min
+//= require sb-admin-2
+//= require bootstrap-datepicker
 //= require login
